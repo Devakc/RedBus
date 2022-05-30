5c7edd8a6e5032d79d87ee2d7b190cace5994c34
Devakc <devkc1705@gmail.com>
2022-05-30 13:01:48 +0530

    first upload

---
+++ src/org/redbusgit/RedBusTask2.java
@@ -0,0 +1,24 @@
+package org.redbusgit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.SendKeysAction;
+
+public class RedBusTask2 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Asus\\eclipse-workspace\\SeleniumProgramming\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.redbus.in/");
+		
+		WebElement txtFrom = driver.findElement(By.id("src"));
+		txtFrom.sendKeys("Chennai");
+		
+        WebElement txtTo = driver.findElement(By.id("dest"));
+        txtTo.sendKeys("Puducherry");
+        
+        
+		
+		
+}}
